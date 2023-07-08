3caeabfe84ad9b775b536a96d5bc41a76d2d92a0
Bruno Duarte <duartebruno2208@gmail.com>
2023-07-08 20:21:14 -0300

    fix: initial copy

--- src/app/initialContent.ts
+++ src/app/initialContent.ts
@@ -1,4 +1,4 @@
-export const initialContent = `<div><h1>Editor Notion</h1><p>Digite ' / ' em nova linha para abrir o menu flutuante ou<br/> selecione um texto existente para abrir botões de estilização de texto<br/><br/>É possível também escrever blocos de código com syntax highlighting :<pre><code class="language-javascript">for (var i=1; i <= 20; i++)
+export const initialContent = `<div><h1>Editor Notion</h1><p>Comece a editar ou digite ' / ' em nova linha para abrir o menu flutuante <br/> selecione um texto para abrir botões de estilização<br/><br/>É possível também escrever blocos de código com syntax highlighting :<pre><code class="language-javascript">for (var i=1; i <= 20; i++)
 {
   if (i % 15 == 0)
     console.log("FizzBuzz");
